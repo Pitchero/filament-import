dcd63b966d8f22e27a1c972a8984315fdbdf4639
James <james@gray.ac>
2024-03-15 11:37:11 +0000

    Initial commit

---
+++ src/Actions/ImportField.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Konnco\FilamentImport\Actions;
+
+use Konnco\FilamentImport\Concerns\HasFieldHelper;
+use Konnco\FilamentImport\Concerns\HasFieldLabel;
+use Konnco\FilamentImport\Concerns\HasFieldMutation;
+use Konnco\FilamentImport\Concerns\HasFieldPlaceholder;
+use Konnco\FilamentImport\Concerns\HasFieldRequire;
+use Konnco\FilamentImport\Concerns\HasFieldValidation;
+
+class ImportField
+{
+    use HasFieldMutation;
+    use HasFieldHelper;
+    use HasFieldPlaceholder;
+    use HasFieldLabel;
+    use HasFieldRequire;
+    use HasFieldValidation;
+
+    public function __construct(private string $name)
+    {
+    }
+
+    public static function make(string $name): self
+    {
+        return new self($name);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
